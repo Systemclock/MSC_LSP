5b1e0f5b855239fe63fd945c226bb960415bff57
Systemclock <2991466716@qq.com>
2024-06-03 19:12:47 +0800

    modify

--- functions/MSCLSP.m
+++ functions/MSCLSP.m
@@ -1,10 +1,10 @@
-function [P,H,Z,S,obj,iter, Allacc] = myLMSC(X,alpha,beta,gama,lamda,epilsion,maxIter,K, kind, Y)
+function [P,H,Z,S,obj,iter] = MSCLSP(X,alpha,beta,gama,lamda,epilsion,maxIter,K)
 % alpha beta gama lamda 超参数
 % output 
-% P 
-% H 
-% Z 
-% S 
+% P the project matrix
+% H latent representation
+% Z self-representation
+% S similarity matrix
 V = size(X,2);    
 N = size(X{1},2); 
 for i=1:V
@@ -16,23 +16,19 @@ for i=1:V
     SD = SD+D{i};                    
     M = [M;X{i}];                  
 end
-% rand('twister',5489);
 % initial
 % H = getPCA(M',K);
 P = zeros(SD,K);
-H = rand(K,N);   % H Z两种初始化方式
+H = rand(K,N);   
 % Z = zeros(N,N);
 Z = rand(N,N);
 S = zeros(N,N);
 obj = [];
 allacc = [];
 iter=1;err=1;
-% 使用高斯函数初始化S 后面两项不起作用时，不跟新S
-
-
-
 while (err>0.00001 && iter<=maxIter)
- 
+
+   % update S
    for i=1:N
        for j=1:N    
              temp(i,j) = exp(-1*gama*(norm(H(:,i)-H(:,j),2)^2)/(lamda+1e-11));   
@@ -46,21 +42,21 @@ while (err>0.00001 && iter<=maxIter)
        S(i,:) = S(i,:)./sum(S(i,:));
    end
    S=(S+S')./2;
-   %
+   % update H
    LapMatrix = diag(sum(S,2))-S;  %L
    A = eye(N)+alpha *(eye(N) - Z - Z' + Z*Z')+gama*LapMatrix;
    C = P'* M;
    H = C/A;
    %% fixed H,P,S update Z
    
-%    iter_Z=1;
-%    while iter_Z<maxIter
-%        Q = diag(0.5./sqrt(sum(Z.*Z,2)+epilsion));
-%        Z = real(alpha * inv(alpha* H'* H+beta*Q)*H'*H);
-%        iter_Z = iter_Z+1;
-%    end
-   Q = diag(0.5./sqrt(sum(Z.*Z,2)+epilsion));
-   Z = real(alpha * inv(alpha* H'* H+beta*Q)*H'*H);
+   iter_Z=1;
+   while iter_Z<maxIter
+       Q = diag(0.5./sqrt(sum(Z.*Z,2)+epilsion));
+       Z = real(alpha * inv(alpha* H'* H+beta*Q)*H'*H);
+       iter_Z = iter_Z+1;
+   end
+%    Q = diag(0.5./sqrt(sum(Z.*Z,2)+epilsion));
+%    Z = real(alpha * inv(alpha* H'* H+beta*Q)*H'*H);
    
    %% fixed Z,H,S update P
    temp_M = M*H';
@@ -90,10 +86,7 @@ while (err>0.00001 && iter<=maxIter)
    if iter>2
         err = abs(obj(iter-1)-obj(iter));
    end
-   Z1 = abs(Z)+abs(Z');
-   [L] = SpectralClustering(Z1,kind);
-   L=L';
-   Allacc(iter) = Accuracy(L',double(Y));
+
    iter = iter+1;
    
 
